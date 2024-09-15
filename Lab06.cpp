669bb6ef6d98ee9df5afa472848d4f4079bab312
RealDev <159386050+Jaturapatpanyadaeng@users.noreply.github.com>
2024-09-15 12:42:09 +0700

    Update Lab06.cpp

--- Lab06.cpp
+++ Lab06.cpp
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    int number;
-    printf( "Enter Your Number: " );
-    scanf( "%d", &number );
+    int number ;
+    printf ( "Enter Your Number: " ) ;
+    scanf ( "%d", &number ) ;
 
     for ( int i = 0; i < number; i++ ) {
-        printf( "\n" );
+        printf ( "\n" ) ;
         for ( int j = 0; j < number; j++ ) {
-            printf( i == (number % 2 == 0 ? j : number - j - 1) ? "1 " : "0 " );
+            printf ( i == ( number % 2 == 0 ? j : number - j - 1 ) ? "1 " : "0 " ) ;
         } //end for
     } // end for
     return 0 ;
